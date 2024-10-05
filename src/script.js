0630f14c84e5b40a2466b576139405339176aff2
Ida <ida.nacka@gmail.com>
2024-10-05 16:11:34 +0200

    clickEffect comment removed

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { createSmokeEffect } from './smokeEffect.js'; // Import the smoke effect function
+import { createSmokeEffect } from './smokeEffect.js'; 
 import { loadFont } from './fontLoader.js';
 import GUI from 'lil-gui';
 
