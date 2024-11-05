8a21bfc7a0b9040c2ff15946ae7e9917a2afa578
Ida <ida.nacka@gmail.com>
2024-11-05 02:57:22 +0100

    clock rotation fixed

--- src/clock.js
+++ src/clock.js
@@ -1,13 +1,21 @@
 export function getCurrentTime() {
     const date = new Date();
+    
     let hours = date.getHours();
     const minutes = date.getMinutes();
     const ampm = hours >= 12 ? 'PM' : 'AM';
     
-    // Convert to 12-hour format
     hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
+    hours = hours ? hours : 12; 
     const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
     
-    return `${hours}:${formattedMinutes} ${ampm}`;
+    const formattedTime = `${hours}:${formattedMinutes} ${ampm}`;
+
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const day = days[date.getDay()];
+    const dayOfMonth = date.getDate();
+    const month = date.getMonth() + 1; 
+    const formattedDate = `${day}.${dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth}.${month < 10 ? '0' + month : month}`;
+
+    return { formattedTime, formattedDate };
 }
