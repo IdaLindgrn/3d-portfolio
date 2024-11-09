d4b2cd0ff25e7c6e62e9bb318c5fdbd855f70a6d
Ida <ida.nacka@gmail.com>
2024-11-09 01:41:30 +0100

    updated bunnyVirusWarning image

--- src/bunnyVirus.js
+++ src/bunnyVirus.js
@@ -62,7 +62,6 @@ export function createBunnyVirusPopup() {
             width: 340px;
             background-color: #E21414;
             color: #FFF;
-            // border: 1px solid #000;
             box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
             z-index: 1;
             display: flex;
