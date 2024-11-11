cc9c998ec71f0625fdcdf322f3cbc65c7b30725a
Ida <ida.nacka@gmail.com>
2024-11-11 21:48:47 +0100

    updated pulse

--- src/shaders/glowingEdges/fragment.glsl
+++ src/shaders/glowingEdges/fragment.glsl
@@ -1,8 +1,8 @@
 uniform float uTime;
 
 void main() {
-  float pulse = abs(sin(uTime * 2.0)) * 0.5 + 0.5;
+  float pulse = abs(sin(uTime * 2.0)) * 0.8 + 0.2;
     vec3 glowColor = vec3(1.0, 1.0, 1.0) * pulse;
-
+    
     gl_FragColor = vec4(glowColor, 1.0);
 }
